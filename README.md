8ff1733da93526f8a024de6830eaa62e52a2fd2e
SajiaAfrin <sajiaafrinmitul05@gmail.com>
2021-11-19 22:03:39 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+![Banner](./assets/images/moneyheist.jpg)
---
+++ lib/utils/constants.dart
@@ -0,0 +1,13 @@
+String netflix = 'assets/images/netflix.png';
+
+String bg = 'assets/images/bg4.jpg';
+
+ String squid = 'assets/images/monheist.png';
+
+String imdb = 'assets/images/imdb-logo.png';
+
+String squid_game = 'assets/images/logo_money.png';
+// String figures = 'assets/images/figures.png';
+double? h;
+
+double? w;
